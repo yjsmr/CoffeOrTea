26fdf0217b82b5ad7b51856aebe24f8d37e1bb9a
Yonas Romell <yonas.romell@live.se>
2020-09-18 10:27:53 +0200

    A commit

---
+++ src/com/example/coffeeortea/CoffeeOrTea.java
@@ -0,0 +1,47 @@
+package com.example.coffeeortea;
+import java.util.Scanner;
+public class CoffeeOrTea {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Coffee or tea, sir? [Write C or T]: ");
+        String choice = scan.nextLine();
+
+        if(choice.equals("C")){
+            System.out.println("Wonderful choice");
+
+            System.out.println("Would you like some cream as well? [Write Y or N]");
+            String creamCoffee = scan.nextLine();
+            if(creamCoffee.equals("Y")){
+                System.out.println("There you go, coffee and cream");
+            }
+            else if(creamCoffee.equals("N")){
+                System.out.println("Black as night it is");
+            }
+            else{
+                System.out.println("Sorry sir, we don't have that");
+            }
+        }
+        else if(choice.equals("T")){
+            System.out.println("Here's your tea");
+
+            System.out.println("Would you like a lemon [Y or N]");
+            String lemon = scan.nextLine();
+            if(lemon.equals("Y")){
+                System.out.println("There you go, tea and a lemon slice. Enjoy!");
+            }
+            else if(lemon.equals("N")){
+                System.out.println("It's not for everyone. Here you go, be careful though, it's quite hot");
+            }
+            else {
+                System.out.println("Sorry sir, we don't have that");
+            }
+
+        }
+        else{
+            System.out.println("Don't be daft, ofc we don't have that");
+        }
+
+    }
+}
